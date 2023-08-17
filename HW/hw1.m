ed0525459feefce6dd7f0d3444336002a0803adf
TheBrianKong <34779986+TheBrianKong@users.noreply.github.com>
2023-08-16 17:27:51 -0700

    Update Q3 and 4
    Fixed some issues, added a test section for when omega = 10 for the modified circuit (PDF)

--- HW/hw1.m
+++ HW/hw1.m
@@ -33,11 +33,19 @@ A= [1   -1  -1   0   0   0;     % I_L - I_Cf - I_Rd = 0
     0    0  -1   0   0  Cd*s];  % -I_Cd + Cd*s*V_mid = 0
 b = [0;0;V_i;0;0;0];
 x=A\b; V_o_PDF =simplify(x(5))
-
-% Q4
+F_PDF = RR_tf([Cd*Rd*s 1],[Cd*Cf*L*Rd (Cd+Cf)*L Cd*Rd 1])
+%% Q4
 Rd = sqrt(L/Cf); Cd = 4*Cf;
-F_PDF = RR_tf([Cd*Rd 1],[Cd*Cf*L*Rd (Cd+Cf)*L Cd*Rd 1]);
-RR_bode(F_PDF);
+omega = 1/(4*sqrt(Cf*L)) % 1/(Cd*Rd) = 1/(4*Cf *(L/Cf)^(1/2)) = 1/(4* C_f^1/2 * L^1/2)
+% F_PDF = RR_tf([Cd*Rd*s 1],[Cd*Cf*L*Rd (Cd+Cf)*L Cd*Rd 1])
+% F_PDF = RR_tf([1 omega],[Cf*L (Cd+Cf)*L*omega 1 omega]) % multiply by omega
+F_PDF = RR_tf([1 omega],[Cf*L 5*Cf*L*omega 1 omega]) % substitute Cd = 4*Cf, 
+
+% TEST: omega = 10 -> Cf*L = 1/1600
+F_PDF_2 = RR_tf([1 10],[1/1600 5/1600*10 1 10])
+RR_bode(F_PDF_2)
+legentries{end+1} = ['PDF (\omega = 10)'];
+legend(legentries);
 %% In class example
 %{
 syms zeta
